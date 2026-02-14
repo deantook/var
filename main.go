ea1107575db37d34369cd6ba5ce4301afdc25a6f
chenyunlong <chenyunlong99@outlook.com>
2026-02-14 10:30:03 +0800

    1

--- main.go
+++ main.go
@@ -1,12 +1,38 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+
+	//从系统变量中获取名为 hello 的变量
+	hello := os.Getenv("hello")
+	fmt.Println("hello", hello)
+	fmt.Println("hello", hello)
+	fmt.Println("hello", hello)
+	fmt.Println("hello", hello)
+	fmt.Println("hello", hello)
+	fmt.Println("hello", hello)
+	fmt.Println("hello", hello)
+	fmt.Println("hello", hello)
+	fmt.Println("hello", hello)
+	fmt.Println("hello", hello)
+
+	// 打印日志
+
+	log.Println("hello", hello)
+	log.Println("hello", hello)
+	log.Println("hello", hello)
+	log.Println("hello", hello)
+	log.Println("hello", hello)
+	log.Println("hello", hello)
+
 	// 创建 gin 路由引擎
 	r := gin.Default()
 
@@ -26,4 +52,27 @@ func main() {
 
 	// 启动服务器，监听 8080 端口
 	r.Run(":8080")
+
+	//从系统变量中获取名为 hello 的变量
+	hello1 := os.Getenv("hello")
+	fmt.Println("hello", hello1)
+	fmt.Println("hello", hello1)
+	fmt.Println("hello", hello1)
+	fmt.Println("hello", hello1)
+	fmt.Println("hello", hello1)
+	fmt.Println("hello", hello1)
+	fmt.Println("hello", hello1)
+	fmt.Println("hello", hello1)
+	fmt.Println("hello", hello1)
+	fmt.Println("hello", hello1)
+
+	// 打印日志1
+
+	log.Println("hello", hello1)
+	log.Println("hello", hello1)
+	log.Println("hello", hello1)
+	log.Println("hello", hello1)
+	log.Println("hello", hello1)
+	log.Println("hello", hello1)
+
 }
